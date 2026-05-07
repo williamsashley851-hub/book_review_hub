b4cfe83d15069af332f03ca99607ff60ebb497ae
Ashley Williams <williamsashley851@gmail.com>
2026-05-07 18:28:32 -0500

    Add validations to Book and Review models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
     has_many :reviews, dependent: :destroy
+    validates :title, :author, :genre, :description, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
+  
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 
+}
 end
