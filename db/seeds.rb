ff98a791d42babf0e2ad580f8c398e7af7d7844c
Ashley Williams <williamsashley851@gmail.com>
2026-05-14 14:28:06 -0500

    Add seed data using Open Library API for 50 books across 5 genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,84 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'net/http'
+require 'json'
+require 'uri'
+
+def fetch_books(query)
+  url = "https://openlibrary.org/search.json?q=#{URI.encode_www_form_component(query)}&limit=10"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  JSON.parse(response)
+ 
+end
+# Fantasy books
+data = fetch_books("fantasy fiction")
+data['docs'].each do |book|
+  title = book['title']
+  author = book['author_name']&.first || "Unknown"
+  genre = "Fantasy"
+  description = "No description available."
+
+  Book.find_or_create_by!(title: title) do |b|
+    b.author = author
+    b.genre = genre
+    b.description = description
+  end
+end
+  # Horror books
+data = fetch_books("horror fiction")
+data['docs'].each do |book|
+  title = book['title']
+  author = book['author_name']&.first || "Unknown"
+  genre = "Horror"
+  description = "No description available."
+
+  Book.find_or_create_by!(title: title) do |b|
+    b.author = author
+    b.genre = genre
+    b.description = description
+  end
+end
+  # Thriller books
+data = fetch_books("thriller fiction")
+data['docs'].each do |book|
+  title = book['title']
+  author = book['author_name']&.first || "Unknown"
+  genre = "Thriller"
+  description = "No description available."
+
+  Book.find_or_create_by!(title: title) do |b|
+    b.author = author
+    b.genre = genre
+    b.description = description
+  end
+end
+  # Autobiography books
+data = fetch_books("autobiography")
+data['docs'].each do |book|
+  title = book['title']
+  author = book['author_name']&.first || "Unknown"
+  genre = "Autobiography"
+  description = "No description available."
+
+  Book.find_or_create_by!(title: title) do |b|
+    b.author = author
+    b.genre = genre
+    b.description = description
+  end
+end
+  # Romance books
+data = fetch_books("romance fiction")
+data['docs'].each do |book|
+  title = book['title']
+  author = book['author_name']&.first || "Unknown"
+  genre = "Romance"
+  description = "No description available."
+
+  Book.find_or_create_by!(title: title) do |b|
+    b.author = author
+    b.genre = genre
+    b.description = description
+  end
+end
